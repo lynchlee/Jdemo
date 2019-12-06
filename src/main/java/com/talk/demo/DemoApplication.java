5f6a8ea26fc3fd791864dfb6b59c8b6a1f608f66
zhangchao11 <zhangchao11@360.cn>
2019-12-06 17:27:20 +0800

    add logger to project

--- src/main/java/com/talk/demo/DemoApplication.java
+++ src/main/java/com/talk/demo/DemoApplication.java
@@ -3,13 +3,18 @@ package com.talk.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 @RestController
 @SpringBootApplication
 public class DemoApplication {
 
+    private static Logger logger = LogManager.getLogger(DemoApplication.class);
+
     @RequestMapping("/")
     String home() {
+        logger.info("this is hello world");
         return "Hello, world!";
     }
 
